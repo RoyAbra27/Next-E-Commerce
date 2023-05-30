024aedf6d7f6b1770dc39fe491128f60f681568c
Yarin Bukai <72891249+byarin90@users.noreply.github.com>
2023-05-30 23:27:51 +0300

    clodinary2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-"use client"
-import Link from "next/link";
 import React, { useEffect } from "react";
-import UploadForm from "./components/test/cloudinary";
 
 const Home = async () => {
  
@@ -10,9 +7,9 @@ const Home = async () => {
   return (
     <div className="">
       <div className="bg-cover bg-center-center  shadow-md h-[60vh] bg-[url(https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/12/19/15/istock-463173435.jpg)]"></div>
-      <UploadForm />
-      <Link href={"/shops"}>shops</Link>
-    </div>
+   
+   sssssssss
+   </div>
   );
 };
 
